d72c8e72d871ae86898166a8ca51686774b8fc23
LAPTOP-C6Q1DJA2\huda prima <hudaprima4@gmail.com>
2022-11-22 19:35:18 +0700

    revisi giitignore

--- storage/framework/views/bc749df55a3148cb135fb82c6627ef21a206862b.php
+++
@@ -1,4 +0,0 @@
-<div id="kt_scrolltop" class="scrolltop" data-kt-scrolltop="true">
-    <i class="fa-duotone fa-jet-fighter-up fa-lg"></i>
-</div>
-<?php /**PATH /Users/yeremiasatrio/Project/wonogiri-fest/resources/views/frontend/templates/tools/scrolltop.blade.php ENDPATH**/ ?>
