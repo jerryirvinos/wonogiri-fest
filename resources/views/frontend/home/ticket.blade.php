797ccc7d0e46c95c566df8d33fc90ac8c6cda388
Yeremia Satrio <>
2022-12-05 20:55:03 +0700

    Make responsive ticket and ticket box on mobile or tab side

--- resources/views/frontend/home/ticket.blade.php
+++ resources/views/frontend/home/ticket.blade.php
@@ -10,36 +10,40 @@
                 </div>
             </div>
             <div class="col-xl-9 col-xxl-10">
-                <div class="row justify-content-center">
+                <div class="d-flex justify-content-center justify-content-lg-between flex-wrap">
                     @foreach ($tickets as $ticket)
-                        <div class="col-lg-4 col-sm-12">
-                            <button type="button"
-                                class="card bg-transparent h-100 mb-5 py-5 {{ $ticket->price ? 'ticketClick' : '' }}"
-                                data-id={{ $ticket->id }} data-price={{ $ticket->price }}>
-                                <div class="bgi-no-repeat bgi-size-contain bgi-position-center position-relative my-auto min-h-175px mw-100 h-auto"
-                                    style="background-image: url('{{ asset('/assets/media/illustrations/tiket3.png') }}')">
-                                    <div class="card-body text-center">
-                                        <div class="d-flex justify-content-start flex-column flex-wrap mb-5">
-                                            <div class="fs-2x text-base font-title">
-                                                {{ $ticket->name }}
-                                            </div>
-                                            <div class="fs-3 fst-italic text-base fw-semibold">
-                                                {{ $ticket->title }} <span class="fs-5">{{ $ticket->detail }}</span>
-                                            </div>
+                        <button type="button"
+                            class="card bg-transparent h-100 py-0 py-lg-5 mx-auto {{ $ticket->price ? 'ticketClick' : '' }}"
+                            data-id={{ $ticket->id }} data-price={{ $ticket->price }}>
+                            <div class="bgi-no-repeat bgi-size-contain bgi-position-center position-relative my-auto min-h-175px mw-100 h-auto"
+                                style="background-image: url('{{ asset('/assets/media/illustrations/tiket3.png') }}')">
+                                <div class="card-body text-center">
+                                    <div class="d-flex justify-content-start flex-column flex-wrap mb-5">
+                                        <div class="fs-2x text-base font-title">
+                                            {{ $ticket->name }}
                                         </div>
-                                        <div class="fs-3x text-base fw-bolder">
-                                            {{ $ticket->price ? format_rupiah($ticket->price) : '' }}
+                                        <div class="fs-3 fst-italic text-base fw-semibold">
+                                            {{ $ticket->title }} <span class="fs-5">{{ $ticket->detail }}</span>
                                         </div>
                                     </div>
+                                    <div class="fs-3x text-base fw-bolder">
+                                        @if ($ticket->price == 0)
+                                            SOLD OUT
+                                        @elseif ($ticket->price == 1)
+                                            &nbsp;
+                                        @else
+                                            {{ $ticket->price ? format_rupiah($ticket->price) : '' }}
+                                        @endif
+                                    </div>
                                 </div>
-                            </button>
-                        </div>
+                            </div>
+                        </button>
                     @endforeach
                 </div>
                 <div class="row justify-content-center">
                     <div class="col-lg-10 col-sm-12">
                         <div
-                            cl3ss="lh-xl fs-8 fs-lg-5fw-semibold mt-4 text-gray-400 text-center mt-5 mt-lg-10 px-5 px-lg-20">
+                            class="lh-xl fs-8 fs-lg-5 fw-semibold mt-4 text-gray-400 text-center mt-5 mt-lg-10 px-5 px-lg-20">
                             Penjualan tiket <span class="fw-bold text-white fs-7 fs-lg-3">Early Bird</span> dilakukan
                             secara offline di Official Ticket Box Wonogiri Festival. <br>
                             Untuk wilayah luar Wonogiri & Sukoharjo bisa melalui hotline : <a
