518673188af57d87b86eedf928090ea18f4d4cde
Yeremia Satrio <>
2022-12-08 10:54:01 +0700

    Rev login interface

--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -1,137 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
-<!--begin::Head-->
 
 <head>
     <base href="../../../" />
     <title>{{ env('APP_NAME') }}</title>
     <link rel="shortcut icon" href="{{ asset('assets/media/logos/favicon.ico') }}" />
     <meta name="_token" content="{{ csrf_token() }}">
-    <!--begin::Fonts(mandatory for all pages)-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
-    <!--end::Fonts-->
-    <!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
     <link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
-    <!--end::Global Stylesheets Bundle-->
+    <link href="{{ asset('assets/css/custom-me.css') }}" rel="stylesheet" type="text/css" />
 </head>
-<!--end::Head-->
-<!--begin::Body-->
 
 <body id="kt_body" class="app-blank bgi-size-cover bgi-position-center bgi-no-repeat">
-    <!--begin::Main-->
-    <!--begin::Root-->
-    <div class="d-flex flex-column flex-root">
-        <!--begin::Page bg image-->
-        <style>
-            body {
-                background-image: url('assets/media/auth/bg10.jpeg');
-            }
-
-            [data-theme="dark"] body {
-                background-image: url('assets/media/auth/bg10-dark.jpeg');
-            }
-        </style>
-        <!--end::Page bg image-->
-        <!--begin::Authentication - Sign-in -->
+    <div class="d-flex flex-column flex-root ">
         <div class="d-flex flex-column flex-lg-row flex-column-fluid">
-            <!--begin::Aside-->
+
             <div class="d-flex flex-lg-row-fluid">
-                <!--begin::Content-->
                 <div class="d-flex flex-column flex-center pb-0 pb-lg-10 p-10 w-100">
-                    <!--begin::Image-->
-                    <img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20"
-                        src="{{ asset('assets/media/auth/agency.png') }}" alt="" />
-                    <img class="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20"
-                        src="{{ asset('assets/media/auth/agency-dark.png') }}" alt="" />
-                    <!--end::Image-->
-                    <!--begin::Title-->
-                    <h1 class="text-gray-800 fs-2qx fw-bold text-center mb-7">WONOGIRI FESTIFAL</h1>
-                    <!--end::Title-->
-                </div>
-                <!--end::Content-->
-            </div>
-            <!--begin::Aside-->
-            <!--begin::Body-->
-            <div class="d-flex flex-column-fluid flex-lg-row-auto justify-content-center justify-content-lg-end p-12">
-                <!--begin::Wrapper-->
-                <div class="bg-body d-flex flex-center rounded-4 w-md-600px p-10">
-                    <!--begin::Content-->
-                    <div class="w-md-400px">
-                        <!--begin::Form-->
+                    <img class="w-25 h-auto img-fluid" src="{{ asset('assets/media/logos/wonogiri-fest-logo.png') }}"
+                        alt="" />
+                    <div class="w-75">
                         <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" action="#">
                             @csrf
-                            <!--begin::Heading-->
                             <div class="text-center mb-11">
-                                <!--begin::Title-->
                                 <h1 class="text-dark fw-bolder mb-3">Sign In</h1>
-                                <!--end::Title-->
                             </div>
-                            <!--begin::Heading-->
-                            <!--begin::Input group=-->
-                            <div class="fv-row mb-8">
-                                <!--begin::Email-->
+                            <div class="fv-row mb-5">
                                 <input type="text" placeholder="Username" name="username" autocomplete="off"
-                                    class="form-control bg-transparent" />
-                                <!--end::Email-->
+                                    class="form-control form-control-lg" />
                             </div>
-                            <!--end::Input group=-->
-                            <div class="fv-row mb-3">
-                                <!--begin::Password-->
+                            <div class="fv-row mb-5">
                                 <input type="password" placeholder="Password" name="password" autocomplete="off"
-                                    class="form-control bg-transparent" />
-                                <!--end::Password-->
+                                    class="form-control form-control-lg" />
                             </div>
-                            <!--end::Input group=-->
-                            <!--begin::Wrapper-->
-                            <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
-                                <div></div>
-                                <!--begin::Link-->
-                                <a href="../../demo20/dist/authentication/layouts/overlay/reset-password.html"
-                                    class="link-primary">Forgot Password ?</a>
-                                <!--end::Link-->
-                            </div>
-                            <!--end::Wrapper-->
-                            <!--begin::Submit button-->
-                            <div class="d-grid mb-10">
-                                <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
-                                    <!--begin::Indicator label-->
-                                    <span class="indicator-label">Sign In</span>
-                                    <!--end::Indicator label-->
-                                    <!--begin::Indicator progress-->
+
+                            <div class="d-grid">
+                                <button type="submit" id="kt_sign_in_submit"
+                                    class="btn btn-warning btn-lg my-10 p-4 rounded-3">
+                                    <span class="indicator-label fs-3 fw-bold">Masuk</span>
                                     <span class="indicator-progress">Please wait...
                                         <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
-                                    <!--end::Indicator progress-->
                                 </button>
                             </div>
-                            <!--end::Submit button-->
                         </form>
-                        <!--end::Form-->
                     </div>
-                    <!--end::Content-->
                 </div>
-                <!--end::Wrapper-->
             </div>
-            <!--end::Body-->
+
+            <div class="d-flex justify-content-center justify-content-lg-end py-8 px-15">
+                <div class="d-flex flex-center rounded-4 w-md-800px bg-base">
+                    <div class="bgi-no-repeat bgi-size-cover bgi-position-center position-relative min-h-100px min-h-lg-200px mw-100 w-100 img-fluid h-100 rounded-4"
+                        style="background-image: url('{{ asset('assets/media/illustrations/login-bg.jpg') }}')">
+                    </div>
+                </div>
+            </div>
+
         </div>
-        <!--end::Authentication - Sign-in-->
     </div>
-    <!--end::Root-->
-    <!--end::Main-->
-    <!--begin::Javascript-->
+
     <script>
         var hostUrl = "assets/";
     </script>
-    <!--begin::Global Javascript Bundle(mandatory for all pages)-->
     <script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
     <script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
-    <!--end::Global Javascript Bundle-->
-    <!--begin::Custom Javascript(used for this page only)-->
+
     @include('frontend.login-script')
-    {{-- <script src="{{ asset('assets/js/custom/authentication/sign-in/general.js')}}"></script> --}}
-    <!--end::Custom Javascript-->
-    <!--end::Javascript-->
 </body>
-<!--end::Body-->
 
 </html>
