6c3d8e828278acaac7116b0f575039191032fcba
Yeremia Satrio <>
2022-12-11 21:34:39 +0700

    Add merchandise section

---
+++ resources/views/frontend/home/merchandise.blade.php
@@ -0,0 +1,27 @@
+<section class="my-5 my-lg-10">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="text-center mb-12">
+                    <div class="fs-4x text-white font-title ls-4 mb-7" id="experience"
+                        data-kt-scroll-offset="{default: 100, lg: 150}">
+                        EXPERIENCE
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center px-8 px-lg-10">
+            <div class="col-xl-10 col-xxl-12">
+                <div class="row">
+                    <div class="col-lg-4 col-sm-4">
+                        <div class="card border border-5 shadow-xs">
+                            <div class="card-body">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
