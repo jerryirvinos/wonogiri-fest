090e80eb53b9bac30e1e9ab4d638685f69082ddb
Yeremia Satrio <>
2022-12-15 07:11:03 +0700

    Deploy remove merch from landing

--- resources/views/frontend/landing.blade.php
+++ resources/views/frontend/landing.blade.php
@@ -33,8 +33,6 @@
 
     @include('frontend.home.experience')
 
-    @include('frontend.home.merchandise')
-
     <section class="my-5 my-lg-10 bg-tagline">
         <div class="d-flex justify-content-center flex-wrap h-100">
             <img src="{{ asset('/assets/media/illustrations/tagline.png') }}" class="mh-auto w-100 mw-100 my-auto"
