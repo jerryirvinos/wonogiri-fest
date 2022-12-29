d7598e9021320f47264bf072dd79fb63ded4cacd
LAPTOP-C6Q1DJA2\huda prima <hudaprima4@gmail.com>
2022-12-29 20:41:15 +0700

    edit file system

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => env('FILESYSTEM_DRIVER', 'public'),
 
     /*
     |--------------------------------------------------------------------------
@@ -67,7 +67,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        env('APP_URL').'/storage' => storage_path('app/public'),
     ],
 
 ];
